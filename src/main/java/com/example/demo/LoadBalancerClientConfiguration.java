6e2fa5fff46dfd9099ba9fa5509d9fd74b508f88
mbrkic4 <nospam@gmail.com>
2020-07-17 09:54:04 +0100

    Initial commit

---
+++ src/main/java/com/example/demo/LoadBalancerClientConfiguration.java
@@ -0,0 +1,28 @@
+package com.example.demo;
+
+import org.springframework.cloud.client.ServiceInstance;
+import org.springframework.cloud.loadbalancer.core.ReactorLoadBalancer;
+import org.springframework.cloud.loadbalancer.core.RoundRobinLoadBalancer;
+import org.springframework.cloud.loadbalancer.core.ServiceInstanceListSupplier;
+import org.springframework.cloud.loadbalancer.support.LoadBalancerClientFactory;
+import org.springframework.context.ConfigurableApplicationContext;
+import org.springframework.context.annotation.Bean;
+import org.springframework.core.env.Environment;
+
+public class LoadBalancerClientConfiguration {
+
+	@Bean
+	public ServiceInstanceListSupplier serviceInstanceListSupplier(ConfigurableApplicationContext context) {
+
+		return ServiceInstanceListSupplier.builder().withDiscoveryClient().withCaching().build(context);
+
+	}
+
+	@Bean
+	public ReactorLoadBalancer<ServiceInstance> serviceInstanceLoadBalancer(Environment environment,
+			LoadBalancerClientFactory loadBalancerClientFactory) {
+		String name = environment.getProperty(LoadBalancerClientFactory.PROPERTY_NAME);
+		return new RoundRobinLoadBalancer(
+				loadBalancerClientFactory.getLazyProvider(name, ServiceInstanceListSupplier.class), name);
+	}
+}
